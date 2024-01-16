c0435a0964a9162640d6376239e92e7ade3a6ec1
Amin Jamali <amin.jamali@broadcom.com>
2024-01-16 19:33:08 +0000

    Switch to ginkgo/v2

--- plugin/runner_test.go
+++ plugin/runner_test.go
@@ -6,7 +6,7 @@ import (
 	"code.cloudfoundry.org/cpu-entitlement-admin-plugin/plugin"
 	"code.cloudfoundry.org/cpu-entitlement-admin-plugin/plugin/pluginfakes"
 	"code.cloudfoundry.org/cpu-entitlement-admin-plugin/reporter"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
